cf0340a7eb6fbdedb2299270e0c6670b1d6f7dbf
weichao <weichao@weichao>
2012-08-17 17:54:24 +0800

    谔谔

--- src/com/wei/test/OOP/common.java
+++ src/com/wei/test/OOP/common.java
@@ -5,6 +5,9 @@ import com.topsoft.tmp.domain.entity.TaskAction;
 public class common {
 
 	public static void main(String e[]){
+		String d = "test01_01.xml";
+		String[] ds= d.split("_");
+		System.out.println(ds.length);
 		TaskAction ta  = new TaskAction();
 		ta.setId(1l);
 		TaskAction ee  = ta;
@@ -13,10 +16,6 @@ public class common {
 		System.out.println(ta.getId());
 		ee.setName("tutu");
 		ee.setId(2l);
-		Long id = ta.getId();
-		id = 3l;
-		String te = ta.getName();
-		te = "hehe";
 		System.out.println(ta.getName());
 		System.out.println(ta.getId());
 	}
