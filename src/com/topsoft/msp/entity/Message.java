6cf9f0914c6ee134c3769529695f44db3e0a8efa
weichao <weichao@weichao>
2012-09-20 10:47:16 +0800

    mannully

--- src/com/topsoft/msp/entity/Message.java
+++ src/com/topsoft/msp/entity/Message.java
@@ -1,5 +1,10 @@
 package com.topsoft.msp.entity;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -10,128 +15,107 @@ import java.util.Date;
  */
 public class Message implements Serializable, Cloneable {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String name; // 名称
-	private Date currentTime;// 当前时间
-	private String clientAddress;// 客户端地址
-	private Long currentOrgId;// 当前机关标识
-	private String currentOrgName;// 当前机关名称
-	private Long currentOperatorId;// 当前操作员标识
-	private String currentOperatorName;//当前操作员姓名
-	private String businessTypeCode;// 业务类型代码
-	private Long bussinessTradeId;// 业务交易标识
-	private String phaseTypeCode;// 环节类型代码
-	private Long phaseTradeId;// 环节交易标识
-	private String operateTypeCode;//操作类型代码
-	private String detailData;
-	private String memo;
-	
+    private String name; // 名称
+    private Date currentTime;// 当前时间
+    private String clientAddress;// 客户端地址
+    private Long currentOrgId;// 当前机关标识
+    private String currentOrgName;// 当前机关名称
+    private Long currentOperatorId;// 当前操作员标识
+    private String currentOperatorName;// 当前操作员姓名
+    private String businessTypeCode;// 业务类型代码
+    private Long bussinessTradeId;// 业务交易标识
+    private String phaseTypeCode;// 环节类型代码
+    private Long phaseTradeId;// 环节交易标识
+    private String operateTypeCode;// 操作类型代码
+    private String detailData;
+    private String memo;
 
-	public String getName()
-	{
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name)
-	{
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Date getCurrentTime()
-	{
-		return currentTime;
-	}
+    public Date getCurrentTime() {
+        return currentTime;
+    }
 
-	public void setCurrentTime(Date currentTime)
-	{
-		this.currentTime = currentTime;
-	}
+    public void setCurrentTime(Date currentTime) {
+        this.currentTime = currentTime;
+    }
 
-	public String getClientAddress()
-	{
-		return clientAddress;
-	}
+    public String getClientAddress() {
+        return clientAddress;
+    }
 
-	public void setClientAddress(String clientAddress)
-	{
-		this.clientAddress = clientAddress;
-	}
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
 
-	public Long getCurrentOrgId()
-	{
-		return currentOrgId;
-	}
+    public Long getCurrentOrgId() {
+        return currentOrgId;
+    }
 
-	public void setCurrentOrgId(Long currentOrgId)
-	{
-		this.currentOrgId = currentOrgId;
-	}
+    public void setCurrentOrgId(Long currentOrgId) {
+        this.currentOrgId = currentOrgId;
+    }
 
-	public String getCurrentOrgName()
-	{
-		return currentOrgName;
-	}
+    public String getCurrentOrgName() {
+        return currentOrgName;
+    }
 
-	public void setCurrentOrgName(String currentOrgName)
-	{
-		this.currentOrgName = currentOrgName;
-	}
+    public void setCurrentOrgName(String currentOrgName) {
+        this.currentOrgName = currentOrgName;
+    }
 
-	public Long getCurrentOperatorId()
-	{
-		return currentOperatorId;
-	}
+    public Long getCurrentOperatorId() {
+        return currentOperatorId;
+    }
 
-	public void setCurrentOperatorId(Long currentOperatorId)
-	{
-		this.currentOperatorId = currentOperatorId;
-	}
+    public void setCurrentOperatorId(Long currentOperatorId) {
+        this.currentOperatorId = currentOperatorId;
+    }
 
-	public String getBusinessTypeCode()
-	{
-		return businessTypeCode;
-	}
+    public String getBusinessTypeCode() {
+        return businessTypeCode;
+    }
 
-	public void setBusinessTypeCode(String businessTypeCode)
-	{
-		this.businessTypeCode = businessTypeCode;
-	}
+    public void setBusinessTypeCode(String businessTypeCode) {
+        this.businessTypeCode = businessTypeCode;
+    }
 
-	public Long getBussinessTradeId()
-	{
-		return bussinessTradeId;
-	}
+    public Long getBussinessTradeId() {
+        return bussinessTradeId;
+    }
 
-	public void setBussinessTradeId(Long bussinessTradeId)
-	{
-		this.bussinessTradeId = bussinessTradeId;
-	}
+    public void setBussinessTradeId(Long bussinessTradeId) {
+        this.bussinessTradeId = bussinessTradeId;
+    }
 
-	public String getPhasesTypeCode()
-	{
-		return phaseTypeCode;
-	}
+    public String getPhasesTypeCode() {
+        return phaseTypeCode;
+    }
 
-	public void setPhasesTypeCode(String phasesTypeCode)
-	{
-		this.phaseTypeCode = phasesTypeCode;
-	}
+    public void setPhasesTypeCode(String phasesTypeCode) {
+        this.phaseTypeCode = phasesTypeCode;
+    }
 
-	public Long getPhasesTradeId()
-	{
-		return phaseTradeId;
-	}
+    public Long getPhasesTradeId() {
+        return phaseTradeId;
+    }
 
-	public void setPhasesTradeId(Long phasesTradeId)
-	{
-		this.phaseTradeId = phasesTradeId;
-	}
+    public void setPhasesTradeId(Long phasesTradeId) {
+        this.phaseTradeId = phasesTradeId;
+    }
 
-	public String getCurrentOperatorName() {
+    public String getCurrentOperatorName() {
         return currentOperatorName;
     }
 
@@ -164,24 +148,36 @@ public class Message implements Serializable, Cloneable {
     }
 
     @Override
-	public String toString()
-	{
-		StringBuilder builder = new StringBuilder("Message{");
-		builder.append("name").append('=').append(name).append(", ");
-		builder.append("currentTime").append('=').append(currentTime).append(", ");
-		builder.append("clientAddress").append('=').append(clientAddress).append(", ");
-		builder.append("currentOrgName").append('=').append(currentOrgName).append(", ");
-		builder.append("currentOperatorName").append('=').append(currentOperatorName).append(", ");
-		builder.append("businessTypeCode").append('=').append(businessTypeCode).append(", ");
-		builder.append("bussinessTradeId").append('=').append(bussinessTradeId).append(", ");
-		builder.append("phasesTypeCode").append('=').append(phaseTypeCode).append(", ");
-		builder.append("phasesTradeId").append('=').append(phaseTradeId).append('}');
-		builder.append("operateTypeCode").append('=').append(phaseTradeId).append('}');
-		return builder.toString();
-	}
+    public String toString() {
+        StringBuilder builder = new StringBuilder("Message{");
+        builder.append("name").append('=').append(name).append(", ");
+        builder.append("currentTime").append('=').append(currentTime).append(", ");
+        builder.append("clientAddress").append('=').append(clientAddress).append(", ");
+        builder.append("currentOrgName").append('=').append(currentOrgName).append(", ");
+        builder.append("currentOperatorName").append('=').append(currentOperatorName).append(", ");
+        builder.append("businessTypeCode").append('=').append(businessTypeCode).append(", ");
+        builder.append("bussinessTradeId").append('=').append(bussinessTradeId).append(", ");
+        builder.append("phasesTypeCode").append('=').append(phaseTypeCode).append(", ");
+        builder.append("phasesTradeId").append('=').append(phaseTradeId).append('}');
+        builder.append("operateTypeCode").append('=').append(phaseTradeId).append('}');
+        return builder.toString();
+    }
 
     @Override
     public Message clone() {
-        return null;
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        Message newMessage = null;
+        try {
+            ObjectOutputStream oo = new ObjectOutputStream(bo);
+            oo.writeObject(this);
+            ByteArrayInputStream bi = new ByteArrayInputStream(bo.toByteArray());
+            ObjectInputStream oi = new ObjectInputStream(bi);
+            newMessage = (Message) oi.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return newMessage;
     }
 }
